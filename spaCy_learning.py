3c2acf87ee7d2f80a3948e04b30979189f912dbf
Bill Nickel <bill.nickel@gmail.com>
2023-09-15 13:35:09 -0700

    Remove extraneous HTML headers, change title.

--- spaCy_learning.py
+++ spaCy_learning.py
@@ -2,13 +2,6 @@ from SpacyAnalyzer import SpacyAnalyzer as sa
 from SpacyAutoMatcher import SpacyAutoMatcher
 import markdown2
 
-#text = "As with dogs from other working breeds, the Australian Cattle Dog is energetic and intelligent with an independent streak. It responds well to structured training, particularly if it is interesting and challenging. It was originally bred to herd by biting, and is known to nip running children. It forms a strong attachment to its owners, and can be protective of them and their possessions. It is easy to groom and maintain, requiring little more than brushing during the shedding period. The most common health problems are deafness and progressive blindness (both hereditary conditions) and accidental injury; otherwise, it is a robust breed with a lifespan of 12 to 16 years."
-#doc = analyzer.nlp(text)
-#analyzer.show_morph(doc)
-
-# https://universaldependencies.org/u/feat/index.html for morph features standards
-# https://en.wikipedia.org/wiki/Imperative_mood
-
 imperative_sentences = [
     "Drive to the store and buy some groceries",
     "Drive to the store",
@@ -30,23 +23,13 @@ Peel and chop the tomatoes, chives and cucumber into very small squares and plac
 """
 
 stanza_matcher = SpacyAutoMatcher("stanza")
-
-#stanza_matches = stanza_matcher.get_imperative_phrases(chatGPT_response)
-#print(stanza_matches)
-
-#stanza_matches = stanza_matcher.get_imperative_phrases_from_sentences(imperative_sentences)
-debug_output = stanza_matcher.debug_for_sentences_as_markdown_table(imperative_sentences)
-
-#convert output to html
-#markdowner = Markdown()
-#html = markdowner.convert(debug_output)
+#debug_output = stanza_matcher.debug_for_sentences_as_markdown_table(imperative_sentences)
+debug_output = stanza_matcher.debug_for_sentences_as_markdown_table([chatGPT_response])
 
 html_debug_template = """<!doctype html>
 <html>
 <head>
-<title>Our Funky HTML Page</title>
-<meta name="description" content="Our first page">
-<meta name="keywords" content="html tutorial template">
+<title>Action Extractor Debug Output</title>
 <style>
 table {
     border-collapse: collapse;
