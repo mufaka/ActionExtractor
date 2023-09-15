f9f07576a08f76b75933bf9dba6557ebef44c977
Bill Nickel <bill.nickel@gmail.com>
2023-09-14 21:41:16 -0700

    Add wikihow pattern keys pickle file Use stanza solution in SpacyAutoMatcher

--- SpacyAutoMatcher.py
+++ SpacyAutoMatcher.py
@@ -1,23 +1,68 @@
 import spacy 
+import stanza 
+import spacy_stanza
 from spacy.matcher import Matcher
+import pickle
 
 class SpacyAutoMatcher:
-    def __init__(self, model_name):
+    def __init__(self, model_name, key_pattern_file):
         self.model_name = model_name
-        self.nlp = spacy.load(model_name)
-    
-    def build_patterns(self, sentences):
-        patterns = []
-        patternKeys = {}
+        
+        # stanza translates to stanza.download("en")
+        if (model_name == "stanza"):
+            self.nlp = self.load_stanza_nlp()
+        else :
+            self.nlp = spacy.load(model_name)
+        
+        if key_pattern_file and key_pattern_file != "":
+            with open(key_pattern_file, 'rb') as f:
+                self.pattern_keys = set(pickle.load(f))
+
+    def load_stanza_nlp():
+        stanza.download("en")
+        nlp_stanza = spacy_stanza.load_pipeline("en")
+        return nlp_stanza
+
+    def get_imperative_phrases(nlp, doc):
+        verb_phrases = []
+        for token in doc:
+            if (token.pos_.lower() == "noun"):
+                ancestors = list(token.ancestors)
+                children = list(token.children)
+                verb_phrase = []
+                if len(ancestors) > 0:
+                    if ancestors[0].pos_.lower() == "verb":
+                        verb_phrase.append(token)
+                        verb_phrase.append(ancestors[0])
+                        if len(children) > 0:
+                            for child in children:
+                                verb_phrase.append(child)
+                        sorted_phrase = sorted(verb_phrase, key=lambda x: x.i)
+                        verb_phrases.append(sorted_phrase)
+
+        imperative_phrases = []
+
+        # is the phrase imperative?
+        for verb_phrase in verb_phrases:
+            phrase = " ".join([x.text for x in verb_phrase])
+            phrase_doc = nlp(phrase)
+            if "Mood=Imp" in phrase_doc[0].morph:
+                imperative_phrases.append(phrase)
+        
+        return imperative_phrases
+
+
+    def build_pattern_keys(self, sentences):
+        keys = []
         for sentence in sentences:
             doc = self.nlp(sentence)
-            key, pattern = self.get_token_pattern(doc)
-            if key not in patternKeys:
-                patternKeys[key] = ''
-                patterns.append(pattern)
-        return patterns                
+            key = self.get_doc_token_key(doc)
+            if (key != ""):
+                if key not in keys:
+                    keys.append(key)
+        return keys
 
-    def get_token_pattern(self, doc):
+    def get_doc_token_key(self, doc):        
         pattern = []
         patternKey = []
 
@@ -31,16 +76,33 @@ class SpacyAutoMatcher:
             else: 
                 tokenDict['POS'] = token.pos_ 
                 patternKey.append(f'POS-{token.pos_}')
-                op = '{1,2}'
+                op = '{1}'
             
             tokenDict['OP'] = op
             pattern.append(tokenDict)
-        
-        return ':'.join(patternKey), pattern 
 
-    def get_matching_phrases(self, exemplarySentences, text):
-        patterns = self. build_patterns(exemplarySentences)
-        return self.get_matching_phrases_by_pattern(patterns, text)
+        return ':'.join(patternKey) if len(pattern) > 2 else ""
+
+    def get_doc_token_keys(self, doc):        
+        pattern = []
+        patternKey = []
+
+        # verbs and nouns use tag_, rest use pos_, OP is always + for now
+        for token in doc:
+            tokenDict = {}
+            op = '+'
+            if token.pos_ == "NOUN" or token.pos_ == "VERB":
+                tokenDict['TAG'] = token.tag_
+                patternKey.append(f'TAG-{token.tag_}')
+            else: 
+                tokenDict['POS'] = token.pos_ 
+                patternKey.append(f'POS-{token.pos_}')
+                op = '{1}'
+            
+            tokenDict['OP'] = op
+            pattern.append(tokenDict)
+
+        return ':'.join(patternKey) if len(pattern) > 2 else ""
 
     def get_matching_phrases_by_pattern(self, patterns, text):
         doc = self.nlp(text)        
@@ -49,3 +111,23 @@ class SpacyAutoMatcher:
         matches = matcher(doc)
         spans = [doc[start:end] for _, start, end in matches]
         return list(spans)
+
+    def get_matching_phrases_by_key(self, text):
+        matches = []
+        doc = self.nlp(text)
+        doc = self.nlp(text)
+        for sent in doc.sents:
+            sent_doc = sent.as_doc()
+            key = self.get_doc_token_key(sent_doc)
+            if key in self.pattern_keys:
+                matches.append(sent.text)
+        return matches
+
+    def get_matching_sentence_by_key(self, sentences):
+        matches = []
+        sentence_docs = list(self.nlp.pipe(sentences))
+        for sent_doc in sentence_docs:
+            key = self.get_doc_token_key(sent_doc)
+            if key in self.pattern_keys:
+                matches.append(sent_doc.text)
+        return matches
--- spaCy_learning.py
+++ spaCy_learning.py
@@ -1,5 +1,9 @@
+import os
+import pickle
 from SpacyAnalyzer import SpacyAnalyzer as sa
 from SpacyAutoMatcher import SpacyAutoMatcher
+import json
+import glob
 
 #analyzer.show_model_info() 
 #analyzer.show_pipeline_info()
@@ -13,13 +17,25 @@ from SpacyAutoMatcher import SpacyAutoMatcher
 
 imperative_sentences = [
     "Drive to the store and buy some groceries",
+    "Drive to the store",
     "Eat the apple if you want",
+    "Eat the apple",
     "Have a nice trip",
     "Go to your cubicle and finish your work",
+    "Go to your cubicle",
+    "finish your work",
     "Hit the ball as hard as you can",
+    "Hit the ball",
     "Update the modifier and resubmit the claim",
-    "Fold the top of the paper downwards"
+    "Fold the top of the paper downwards",
+    "Fold the top two corners of the paper"
 ]
+
+#auto_matcher = SpacyAutoMatcher("en_core_web_trf")
+#keys = auto_matcher.build_pattern_keys(imperative_sentences)
+#print(keys)
+#quit()
+
 
 #imperative_phrases = analyzer.get_imperative_phrases_for_sentences(imperative_sentences)
 #print(*imperative_phrases, sep = "\n")
@@ -27,18 +43,7 @@ imperative_sentences = [
 # gpt prompt: "You are an origami teacher teaching students how to make a paper airplane. Give them all of the steps required to make an airplane."
 
 chatGPT_response = """
-1. Start with a rectangular sheet of paper.
-2. Fold the top two corners of the paper towards the center.
-3. Turn the paper over and fold the top two corners of the paper towards the center again.
-4. Flip the paper over again and fold the top of the paper downwards, so that the two flaps created in the previous steps meet in the middle.
-5. Fold the paper in half along the line created by the two flaps.
-6. Unfold the paper, and then fold the top corners of the paper inwards again.
-7. Turn the paper over and fold the top corners of the paper inwards again.
-8. Fold the two sides of the paper inwards, so that they meet in the middle.
-9. Flip the paper over and fold the two sides of the paper inwards again.
-10. Fold the front of the paper downwards, so that the two flaps created in the previous steps meet in the middle.
-11. Fold the wings of the paper plane downwards.
-12. Your paper plane is now finished!
+Peel and chop the tomatoes, chives and cucumber into very small squares and place in a salad bowl. – Wash, dry and chop the herbs equally and add to the salad bowl. – Let the couscous soak for a few minutes until it becomes fluffy. Then add to the mix. – Pour the oil, add the salt and sprinkle with lemon, then stir everything. – Cover the salad bowl and refrigerate two hours before serving.
 """
 
 # what if we build matcher patterns on the fly from exemplary phrases?
@@ -46,9 +51,63 @@ chatGPT_response = """
 #patterns = analyzer.build_patterns(imperative_sentences)
 #print(patterns)
 
+# can we load the wikiHow data?
+# /mnt/d/ML/Datasets/WIKIHOW/archive/wikiHow0.json
+
+'''
+with open('action_patterns.pkl', 'rb') as f:
+    patterns = pickle.load(f)
+    first_100 = patterns[:100]
+
+    for pattern in first_100:
+        print(pattern)
+'''
+
+# too many patterns means slow AF matching in spaCy ...
+# [{'TAG': 'VB', 'OP': '+'}, {'POS': 'ADP', 'OP': '{1}'}, {'POS': 'DET', 'OP': '{1}'}, {'TAG': 'NN', 'OP': '+'}, {'POS': 'ADP', 'OP': '{1}'}, {'POS': 'DET', 'OP': '{1}'}, {'POS': 'ADJ', 'OP': '{1}'}, {'TAG': 'NN', 'OP': '+'}, {'POS': 'PUNCT', 'OP': '{1}'}]
+
+'''
+wikihow_statements = []
+
+wikihow_glob = f'/mnt/d/ML/Datasets/WIKIHOW/archive/wikiHow*.json'
+wikihow_files = glob.glob(wikihow_glob)
+
+for file in wikihow_files:
+    with open(file) as wikiHow:
+        data = json.load(wikiHow)
+        for article in data:
+            if "Parts" in article:
+                for parts in article["Parts"]:
+                    if "steps" in parts:
+                        for step in parts["steps"]:       
+                            if "Headline" in step:
+                                wikihow_statements.append(step["Headline"])
+
 auto_matcher = SpacyAutoMatcher("en_core_web_trf")
-actions_from_text = auto_matcher.get_matching_phrases(imperative_sentences, chatGPT_response)
-print(*actions_from_text, sep = "\n")
+keys = auto_matcher.build_pattern_keys(wikihow_statements)
+
+with open('action_pattern_keys.pkl', 'wb') as f:
+    pickle.dump(keys, f)
+
+patterns = []
+with open('action_pattern_keys.pkl', 'rb') as f:
+    patterns = pickle.load(f)
+'''
+
+auto_matcher = SpacyAutoMatcher("en_core_web_trf", "wikihow_action_pattern_keys.pkl")
+matches = auto_matcher.get_matching_phrases_by_key(chatGPT_response)
+print("---- TEXT MATCHES ----")
+print(matches)
+print()
+
+sentence_matches = auto_matcher.get_matching_sentence_by_key(imperative_sentences)
+print("---- SENTENCE MATCHES ----")
+print(sentence_matches)
+
+
+
+#actions_from_text = auto_matcher.get_matching_phrases(wikihow_statements, chatGPT_response)
+#print(*actions_from_text, sep = "\n")
 
 #analyzer = sa("en_core_web_lg")
 #actions_from_text = analyzer.get_matching_phrases(imperative_sentences, chatGPT_response)
